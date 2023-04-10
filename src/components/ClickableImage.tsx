4bacc567d35125d52fcbeeb1e803d413befada1a
js <50718474+jarell-santella@users.noreply.github.com>
2023-04-09 22:57:46 -0700

    implementation of clickable image component

---
+++ src/components/ClickableImage.tsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect, useRef } from "react"
+import {
+  StyledImage,
+  StyledBackground,
+  StyledImageModal,
+  StyledCloseIcon,
+} from "./styles/ClickableImage.styled"
+
+interface ClickableImageProps {
+  src: string
+  alt: string
+  width?: string
+  height?: string
+}
+
+const ClickableImage = ({ src, alt, width, height }: ClickableImageProps) => {
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const [showCursor, setShowCursor] = useState<boolean>(true)
+  const [widthLongest, setWidthLongest] = useState<boolean>(true)
+
+  const imageRef = useRef<HTMLImageElement>(null)
+
+  useEffect(() => {
+    if (showModal) {
+      document.body.style.overflow = "hidden"
+    } else {
+      document.body.style.overflow = "visible"
+    }
+  }, [showModal])
+
+  const handleSettingWidthLongest = () => {
+    const image = imageRef.current!
+    if (image.naturalWidth >= image.naturalHeight) {
+      const scaleFactor = (window.innerWidth * 0.8) / image.naturalWidth
+      image.naturalHeight * scaleFactor >= window.innerHeight * 0.8
+        ? setWidthLongest(false)
+        : setWidthLongest(true)
+    } else {
+      const scaleFactor = (window.innerHeight * 0.8) / image.naturalHeight
+      image.naturalWidth * scaleFactor >= window.innerWidth * 0.8
+        ? setWidthLongest(true)
+        : setWidthLongest(false)
+    }
+  }
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | undefined
+
+    const handleMouseMove = () => {
+      setShowCursor(true)
+
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+        timeoutId = undefined
+      }
+
+      timeoutId = setTimeout(() => {
+        setShowCursor(false)
+      }, 4000)
+    }
+
+    window.addEventListener("mousemove", handleMouseMove)
+    window.addEventListener("resize", handleSettingWidthLongest)
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("resize", handleSettingWidthLongest)
+    }
+  }, [])
+
+  return (
+    <>
+      <StyledImage
+        ref={imageRef}
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        onClick={() => {
+          setShowModal(true)
+        }}
+        onLoad={() => {
+          handleSettingWidthLongest()
+        }}
+        showCursor={showCursor}
+      />
+      <StyledBackground
+        showModal={showModal}
+        onClick={() => {
+          setShowModal(false)
+        }}
+      >
+        <StyledImageModal
+          src={src}
+          alt={alt}
+          onClick={(event) => {
+            event.stopPropagation()
+          }}
+          widthLongest={widthLongest}
+        />
+        <StyledCloseIcon
+          onClick={() => {
+            setShowModal(false)
+          }}
+        />
+      </StyledBackground>
+    </>
+  )
+}
+
+export default ClickableImage
