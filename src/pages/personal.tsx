590855f834323e0ffe801483fdd32746c6064bae
js <50718474+jarell-santella@users.noreply.github.com>
2023-04-13 23:05:04 -0700

    placeholders implementation of personal page

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -2,13 +2,47 @@ import * as React from "react"
 import type { HeadFC } from "gatsby"
 import { GlobalStyle } from "../components/styles/GlobalStyles.styled"
 import NavBar from "../components/NavBar"
+import { StyledMediaContainer } from "../components/styles/PersonalPage.styled"
+import MasonryLayout from "../components/MasonryLayout"
+import ClickableImage from "../components/ClickableImage"
+import imageOne from "../images/placeholders/16x10placeholder.png"
+import imageTwo from "../images/placeholders/14x10placeholder.png"
+import imageThree from "../images/placeholders/4x3placeholder.png"
+import imageFour from "../images/placeholders/16x9placeholder.png"
+import imageFive from "../images/placeholders/1.85x1placeholder.png"
+import imageSix from "../images/placeholders/3x2placeholder.png"
+import imageSeven from "../images/placeholders/2x1placeholder.png"
+import imageEight from "../images/placeholders/1x1placeholder.png"
+import imageNine from "../images/placeholders/5x4placeholder.png"
+
+const media = [
+  imageOne,
+  imageTwo,
+  imageThree,
+  imageFour,
+  imageFive,
+  imageSix,
+  imageSeven,
+  imageEight,
+  imageNine,
+]
 
 const PersonalPage = () => {
   return (
     <>
       <GlobalStyle />
       <NavBar />
-      <h1 style={{margin: "0px"}}>Welcome to the PERSONAL tab</h1>
+      <StyledMediaContainer>
+        <MasonryLayout columnWidth={400} spacing={20}>
+          {media.map((media, index) => (
+            <ClickableImage
+              key={index}
+              src={media}
+              alt={`Personal media ${index}`}
+            />
+          ))}
+        </MasonryLayout>
+      </StyledMediaContainer>
     </>
   )
 }
