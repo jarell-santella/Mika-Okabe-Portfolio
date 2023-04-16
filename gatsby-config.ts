78b4c7d480ffd3a4b31ca2da602ee7f3b926470c
js <50718474+jarell-santella@users.noreply.github.com>
2023-04-16 15:01:04 -0700

    added site metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,44 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Mika Okabe Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Mika's Portfolio`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Curabitur eget diam id sapien interdum posuere. Maecenas
+                  vitae sem sed nulla malesuada aliquet in quis ligula. Sed
+                  commodo ornare magna libero.`,
+    author: `Mika Okabe`,
+    creator: `Jarell Santella`,
+    type: `website`,
+    siteUrl: `https://mikaokabe.com`,
+    keywords: `artist, art, storyboard, animation, portfolio, panels, tv,
+               television, commission, freelance, film, beats, illustration`,
+    openGraphImage: `/images/1200x630placeholder.png`,
+    twitterImage: `/images/1200x600placeholder.png`,
+    twitterUsername: `@wishterias`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/favicon1000.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon1000.png",
+      },
     },
-    __key: "images"
-  }]
-};
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+}
 
-export default config;
+export default config
